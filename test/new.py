df2ec69aea42dff85f17c807274b4a8c4c27fd10
Jack Robbers <jack@2pisoftware.com>
2020-10-22 19:19:29 +1100

    try example from docs

--- test/new.py
+++ test/new.py
@@ -1,28 +1,41 @@
 from pysb import *
 import matplotlib.pyplot as plt
+import numpy as np
+
 Model()
 
-Monomer('ROS')
+Monomer('A', ['b'])
+Parameter('A_0', 2.0)
 
-Initial(ROS(), Parameter('t0_ROS', 1))
-Parameter('m', 2)
-Observable('ROS_total', ROS())
-Expression('k20', ROS_total * m)
+Initial(A(b=None), A_0)
+Parameter('A_multiplier', 2.0)
+Observable('A_total', A())
+Expression('kf_A', A_total * A_multiplier)
+Rule('bindA', A(b=None) + A(b=None) >> A(b=1) % A(b=1), kf_A)
 
-Rule('ROS_production', None >> ROS(), k20)
+# Monomer('ROS')
 
-from pysb.simulator import ScipyOdeSimulator, KappaSimulator
+# Initial(ROS(), Parameter('t0_ROS', 1))
+# Parameter('m', 2)
+# Observable('ROS_total', ROS())
+# Expression('k20', ROS_total * m)
+
+# Rule('ROS_production', None >> ROS(), k20)
+
+from pysb.simulator import ScipyOdeSimulator, KappaSimulator, BngSimulator
 
 from pysb.export import export
 
-with open('model.kappa', 'w') as outfile:
-    outfile.write(export(model, 'kappa'))
+# with open('model.kappa', 'w') as outfile:
+#     outfile.write(export(model, 'kappa'))
 
-simres = KappaSimulator(model, tspan = [0,1,2,3,4,5,6,7,8,9,10]).run()
-yout = simres.all
+sim = BngSimulator(model)
+simres = sim.run(tspan=np.linspace(1, 1000), verbose=False, n_runs=1, method='ssa')
+
+# simres = KappaSimulator(model, tspan = [0,1,2,3,4,5,6,7,8,9,10]).run()
+# yout = simres.all
 
 fig, axs = plt.subplots(3, 1)
 
 axs[0].plot(t, yout['ROS_total'], label="ROS")
 fig.savefig('a.png')
-
